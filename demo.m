87c7d8765dd5281d719584e25a441cee56467ef0
Turnin <2259824474@qq.com>
2022-10-28 15:45:38 +0800

    目标声压为靠近亮区的声源全中为0.5，绘制的图像在低频时与文献相似

---
+++ demo.m
@@ -0,0 +1,27 @@
+clc
+clear 
+
+%% 绘制出不同声源间距时的结果，频率步长50Hz，声场采样点间隔0.01m
+[F,A]=PM(0.1,50);
+semilogx(F,A,'r','DisplayName','D=0.1m');
+hold on
+[F,A]=PM(0.2,50);
+semilogx(F,A,'g','DisplayName','D=0.2m');
+hold on
+[F,A]=PM(0.4,50);
+semilogx(F,A,'b','DisplayName','D=0.4m');
+hold on
+[F,A]=PM(0.6,50);
+semilogx(F,A,'k--','DisplayName','D=0.6m');
+hold on
+[F,A]=PM(0.8,50);
+semilogx(F,A,'k-.','DisplayName','D=0.8m');
+hold on
+[F,A]=PM(1,50);
+semilogx(F,A,'k:','DisplayName','D=1m');
+hold off
+axis([100 10000 0 50])
+legend
+
+xlabel('Frequency,Hz')
+ylabel('ContrastPerformance,dB')
