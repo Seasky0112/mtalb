8b8b2f756d9160ad38c3564e9d457e73c7afbc26
Turnin <2259824474@qq.com>
2022-11-04 10:25:41 +0800

    调用ACC4函数，计算ACC方法的声学对比度和Array Effort

--- demo.m
+++ demo.m
@@ -1,27 +1,61 @@
 clc
 clear 
 
-%% 绘制出不同声源间距时的结果，频率步长50Hz，声场采样点间隔0.01m
-[F,A]=PM(0.1,50);
-semilogx(F,A,'r','DisplayName','D=0.1m');
+%% 绘制出不同声源间距时的结果
+[F,ACC,AE]=PM(0.1,10);
+f1=figure;
+semilogx(F,ACC,'r','DisplayName','D=0.1m');
 hold on
-[F,A]=PM(0.2,50);
-semilogx(F,A,'g','DisplayName','D=0.2m');
+f2=figure;
+semilogx(F,AE,'r','DisplayName','D=0.1m');
 hold on
-[F,A]=PM(0.4,50);
-semilogx(F,A,'b','DisplayName','D=0.4m');
+
+[F,ACC,AE]=PM(0.2,10);
+figure(f1) 
+semilogx(F,ACC,'g','DisplayName','D=0.2m');
 hold on
-[F,A]=PM(0.6,50);
-semilogx(F,A,'k--','DisplayName','D=0.6m');
+figure(f2)
+semilogx(F,AE,'g','DisplayName','D=0.2m');
 hold on
-[F,A]=PM(0.8,50);
-semilogx(F,A,'k-.','DisplayName','D=0.8m');
+
+[F,ACC,AE]=PM(0.4,10);
+figure(f1)
+semilogx(F,ACC,'b','DisplayName','D=0.4m');
 hold on
-[F,A]=PM(1,50);
-semilogx(F,A,'k:','DisplayName','D=1m');
+figure(f2)
+semilogx(F,AE,'b','DisplayName','D=0.4m');
+hold on
+
+[F,ACC,AE]=PM(0.6,10);
+figure(f1)
+semilogx(F,ACC,'k--','DisplayName','D=0.6m');
+hold on
+figure(f2)
+semilogx(F,AE,'k--','DisplayName','D=0.6m');
+hold on
+
+[F,ACC,AE]=PM(0.8,10);
+figure(f1)
+semilogx(F,ACC,'k-.','DisplayName','D=0.8m');
+hold on
+figure(f2)
+semilogx(F,AE,'k-','DisplayName','D=0.8m');
+hold on
+
+[F,ACC,AE]=PM(1,10);
+figure(f1)
+semilogx(F,ACC,'k:','DisplayName','D=1m');
 hold off
 axis([100 10000 0 50])
 legend
-
 xlabel('Frequency,Hz')
 ylabel('ContrastPerformance,dB')
+
+figure(f2)
+semilogx(F,AE,'k:','DisplayName','D=1m');
+hold off
+axis([100 10000 -20 60])
+legend
+
+xlabel('Frequency,Hz')
+ylabel('Array Effort,dB')
